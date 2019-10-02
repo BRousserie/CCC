bc756d1d73a0e5d748dee66f22889baec6fc0723
brousserie <b.rousserie24@gmail.com>
2019-10-02 17:26:59 +0200

    Added Transforms

--- CCC/src/CCC/Renderer/Renderer.h
+++ CCC/src/CCC/Renderer/Renderer.h
@@ -17,10 +17,12 @@ namespace CCC
 		
 		inline static void EndScene() {}
 
-		inline static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+		inline static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f))
 		{
 			shader->Bind();
 			shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+			shader->UploadUniformMat4("u_Transform", transform);
+			
 			vertexArray->Bind();
 			RenderCommand::DrawIndexed(vertexArray);
 		}
--- Garage/src/GarageApp.cpp
+++ Garage/src/GarageApp.cpp
@@ -2,11 +2,13 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public CCC::Layer
 {
 public:
 	ExampleLayer()
-		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), Layer("Example"), m_CameraPosition(0.0f)
+		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), Layer("Example"), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
 	{
 
 #pragma region Triangle
@@ -18,7 +20,7 @@ public:
 			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
 			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
 		};
-		
+
 		std::shared_ptr<CCC::VertexBuffer> triangleVertexBuffer;
 		triangleVertexBuffer.reset(CCC::VertexBuffer::Create(vertices, sizeof(vertices)));
 
@@ -31,9 +33,10 @@ public:
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		std::shared_ptr<CCC::IndexBuffer> triangleIndexBuffer;
-		triangleIndexBuffer.reset(CCC::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		triangleIndexBuffer.reset(CCC::IndexBuffer::Create(
+			indices, sizeof(indices) / sizeof(uint32_t)));
 		m_VertexArray->SetIndexBuffer(triangleIndexBuffer);
-		
+
 		std::string vertexSrc = R"(
 			#version 330 core
 
@@ -41,6 +44,7 @@ public:
 			layout (location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -49,7 +53,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 	
 		)";
@@ -76,37 +80,38 @@ public:
 #pragma region Square
 		m_SquareVA.reset(CCC::VertexArray::Create());
 		float sqrVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f,
 		};
 		std::shared_ptr<CCC::VertexBuffer> squareVB;
 		squareVB.reset(CCC::VertexBuffer::Create(sqrVertices, sizeof(sqrVertices)));
-		
+
 		squareVB->SetLayout({
 			{ CCC::ShaderDataType::Float3, "a_Position" }
-		});
+			});
 		m_SquareVA->AddVertexBuffer(squareVB);
 
-		uint32_t sqrIndices[6] = { 0, 1, 2, 2, 3, 0};
+		uint32_t sqrIndices[6] = { 0, 1, 2, 2, 3, 0 };
 		std::shared_ptr<CCC::IndexBuffer> squareIB;
 		squareIB.reset(CCC::IndexBuffer::Create(sqrIndices, sizeof(sqrIndices) / sizeof(uint32_t)));
 		m_SquareVA->SetIndexBuffer(squareIB);
-		
+
 		std::string coloredOnPositionVertexSrc = R"(
 			#version 330 core
 
 			layout (location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 		
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 	
 		)";
@@ -127,27 +132,37 @@ public:
 
 		m_coloredOnPositionShader.reset(new CCC::Shader(coloredOnPositionVertexSrc, coloredOnPositionFragmentSrc));
 #pragma endregion
-		
+
 	}
 	void OnUpdate(CCC::Timestep ts) override
 	{
 		CCC_TRACE("Delta time : {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
-		
-		if(CCC::Input::IsKeyPressed(CCC_KEY_LEFT))
+
+		if (CCC::Input::IsKeyPressed(CCC_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
 		else if (CCC::Input::IsKeyPressed(CCC_KEY_RIGHT))
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
-		
+
 		if (CCC::Input::IsKeyPressed(CCC_KEY_UP))
 			m_CameraPosition.y += m_CameraMoveSpeed * ts;
 		else if (CCC::Input::IsKeyPressed(CCC_KEY_DOWN))
 			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
-		
+
 		if (CCC::Input::IsKeyPressed(CCC_KEY_Q))
 			m_CameraRotation += m_CameraRotateSpeed * ts;
 		if (CCC::Input::IsKeyPressed(CCC_KEY_D))
 			m_CameraRotation -= m_CameraRotateSpeed * ts;
-		
+
+		if (CCC::Input::IsKeyPressed(CCC_KEY_J))
+			m_SquarePosition.x -= m_SquareMoveSpeed * ts;
+		else if (CCC::Input::IsKeyPressed(CCC_KEY_L))
+			m_SquarePosition.x += m_SquareMoveSpeed * ts;
+
+		if (CCC::Input::IsKeyPressed(CCC_KEY_I))
+			m_SquarePosition.y += m_SquareMoveSpeed * ts;
+		else if (CCC::Input::IsKeyPressed(CCC_KEY_K))
+			m_SquarePosition.y -= m_SquareMoveSpeed * ts;
+
 		CCC::RenderCommand::SetClearColor({ 0.1, 0.2, 0.3, 1 });
 		CCC::RenderCommand::Clear();
 
@@ -155,8 +170,18 @@ public:
 		m_Camera.SetPosition({ m_CameraPosition });
 
 		CCC::Renderer::BeginScene(m_Camera);
-		CCC::Renderer::Submit(m_coloredOnPositionShader, m_SquareVA);
-		CCC::Renderer::Submit(m_Shader, m_VertexArray);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				CCC::Renderer::Submit(m_coloredOnPositionShader, m_SquareVA, transform);
+			}
+		}
+		//CCC::Renderer::Submit(m_Shader, m_VertexArray);
 		CCC::Renderer::EndScene();
 	}
 
@@ -167,19 +192,22 @@ public:
 	void OnEvent(CCC::Event& event) override
 	{
 	}
-	
+
 private:
 	std::shared_ptr<CCC::Shader> m_Shader;
 	std::shared_ptr<CCC::VertexArray> m_VertexArray;
-	
+
 	std::shared_ptr<CCC::Shader> m_coloredOnPositionShader;
 	std::shared_ptr<CCC::VertexArray> m_SquareVA;
-	
+
 	CCC::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
 	float m_CameraRotation = 0.0f;
 	float m_CameraMoveSpeed = 5.0f;
 	float m_CameraRotateSpeed = 90.0f;
+
+	glm::vec3 m_SquarePosition;
+	float m_SquareMoveSpeed = 2.0f;
 };
 
 class Garage : public CCC::Application
